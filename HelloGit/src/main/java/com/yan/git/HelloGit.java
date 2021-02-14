c13dcce6a7657506e8d370d3b351eff1dadb2e9d
yanming <1670045010@qq.com>
2021-02-14 20:17:22 +0800

    idea commit 1

--- HelloGit/src/main/java/com/yan/git/HelloGit.java
+++ HelloGit/src/main/java/com/yan/git/HelloGit.java
@@ -8,5 +8,6 @@ package com.yan.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git 2");
     }
 }
